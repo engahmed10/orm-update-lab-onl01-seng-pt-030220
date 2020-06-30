d6f0a1c82509366ec4152b062836bf91c8285cf2
AHMED AFANDY <eng.computer1@yahoo.com>
2020-06-30 18:18:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,9 +17,10 @@ class Student
   end
 
   def self.drop_table
-    sql=<<-SQL
-    DROP TABLE students
-    SQL
+    #sql=<<-SQL
+    sql="DROP TABLE
+    students"
+    #SQL
     DB[:conn].execute(sql)
   end
 
